6c4576e460e25669e073625233a3f4dd34f8c1a1
Gustavo <gustavo.gigante.s@gmail.com>
2019-06-12 23:14:01 +0100

    Services screen + contact Me Screen(wip yet)

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -10,7 +10,7 @@
  */
 get_header();
 ?>
-<div id="homeScreen" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/background-image.jpg);'">
+<div id="homeScreen" class="container mw-100 p-0" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/background-image.jpg);'">
     <div class="bgImg"></div>
     <div class="w-100 nameTitle">
         <h2>
@@ -31,6 +31,64 @@ get_header();
         <span class="learnMoreCopy">Learn more about me</span>
         <span class="learnMoreArrow"><img src="<?php echo get_template_directory_uri(); ?>/images/arrow.svg"></span>
     </div>
+</div>
+
+<div id="servicesScreen" class="container">
+
+    <div class="row servicesTitle">
+        <h3 class="m-auto">SERVICES</h3>
+    </div>
+
+    <div class="servicesWrapper row">
+        <div class="service col-lg-4 col-md-12 text-center">
+            <span class="d-block"><img src="<?php echo get_template_directory_uri(); ?>/images/translation-icon.svg"></span>
+            <h2>Translation</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta nec velit vel tincidunt. Praesent hendrerit, tellus nec pellentesque viverra, lectus lectus molestie risus
+            </p>
+            <button>Contact Me</button>
+        </div>
+        <div class="service col-lg-4 col-md-12 text-center">
+            <span class="d-block"><img src="<?php echo get_template_directory_uri(); ?>/images/proofreading-icon.svg"></span>
+            <h2>ProofReading</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta nec velit vel tincidunt. Praesent hendrerit, tellus nec pellentesque viverra, lectus lectus molestie risus
+            </p>
+            <button>Contact Me</button>
+        </div>
+        <div class="service col-lg-4 col-md-12 text-center">
+            <span class="d-block"><img src="<?php echo get_template_directory_uri(); ?>/images/interpretation-icon.svg"></span>
+            <h2>Interpretation</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta nec velit vel tincidunt. Praesent hendrerit, tellus nec pellentesque viverra, lectus lectus molestie risus
+            </p>
+            <button>Contact Me</button>
+        </div>
+    </div>
+
+</div>
+
+<div id="contactScreen" class="container mw-100" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/background-image.jpg);'">
+
+    <div class="row contactTitle">
+        <h3 class="m-auto">CONTACT ME</h3>
+    </div>
+
+    <div class="row m-0">
+        <form action="/action_page.php" class="m-auto">
+            <span>First name:</span><br>
+            <input type="text" name="firstname">
+            <br>
+            <span>Last name:</span><br>
+            <input type="text" name="lastname">
+            <br>
+            <span>Message:</span><br>
+            <textarea name="message"></textarea>
+            <br><br>
+            <input type="submit" value="Submit">
+        </form> 
+    </div>
+
 </div>
 
 <?php get_footer(); ?>
